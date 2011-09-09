c9ea25ad720218dc2e55fb233caa0940364b03f3
John Byrd <jbyrd@giganticsoftware.com>
2011-09-09 15:24:35 -0700

    Initial checkin.

---
+++ navtree.js
@@ -0,0 +1,367 @@
+var NAVTREE =
+[
+  [ "logog", "index.html", [
+    [ "logog - logger optimized for games", "index.html", null ],
+    [ "Related Pages", "pages.html", [
+      [ "Unit test framework", "unittesting.html", null ],
+      [ "Quick start", "quickstart.html", null ],
+      [ "Internal architecture", "architecture.html", null ],
+      [ "Porting logog", "porting.html", null ],
+      [ "Requirements", "requirements.html", null ],
+      [ "License agreement", "license.html", null ],
+      [ "Other logging frameworks and systems", "othersystems.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "Allocator< T >", "classlogog_1_1_allocator.html", null ],
+      [ "Cerr", "classlogog_1_1_cerr.html", null ],
+      [ "Checkpoint", "classlogog_1_1_checkpoint.html", null ],
+      [ "Cout", "classlogog_1_1_cout.html", null ],
+      [ "Filter", "classlogog_1_1_filter.html", null ],
+      [ "Formatter", "classlogog_1_1_formatter.html", null ],
+      [ "FormatterGCC", "classlogog_1_1_formatter_g_c_c.html", null ],
+      [ "FormatterMSVC", "classlogog_1_1_formatter_m_s_v_c.html", null ],
+      [ "INIT_PARAMS", "structlogog_1_1_i_n_i_t___p_a_r_a_m_s.html", null ],
+      [ "LockableNodesType", "classlogog_1_1_lockable_nodes_type.html", null ],
+      [ "LogOutput", "classlogog_1_1_log_output.html", null ],
+      [ "Message", "classlogog_1_1_message.html", null ],
+      [ "Mutex", "classlogog_1_1_mutex.html", null ],
+      [ "Node", "classlogog_1_1_node.html", null ],
+      [ "Object", "classlogog_1_1_object.html", null ],
+      [ "OutputDebug", "classlogog_1_1_output_debug.html", null ],
+      [ "Allocator< T >::rebind< U >", "structlogog_1_1_allocator_1_1rebind.html", null ],
+      [ "ScopedLock", "classlogog_1_1_scoped_lock.html", null ],
+      [ "Socket", "classlogog_1_1_socket.html", null ],
+      [ "SocketServer", "classlogog_1_1_socket_server.html", null ],
+      [ "Statics", "classlogog_1_1_statics.html", null ],
+      [ "String", "classlogog_1_1_string.html", null ],
+      [ "Target", "classlogog_1_1_target.html", null ],
+      [ "TestSignup", "classlogog_1_1_test_signup.html", null ],
+      [ "Thread", "classlogog_1_1_thread.html", null ],
+      [ "Timer", "classlogog_1_1_timer.html", null ],
+      [ "Topic", "classlogog_1_1_topic.html", null ],
+      [ "TopicGroup", "classlogog_1_1_topic_group.html", null ],
+      [ "TopicLevel", "classlogog_1_1_topic_level.html", null ],
+      [ "TopicSink", "classlogog_1_1_topic_sink.html", null ],
+      [ "TopicSource", "classlogog_1_1_topic_source.html", null ],
+      [ "UnitTest", "classlogog_1_1_unit_test.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "Allocator< T >", "classlogog_1_1_allocator.html", null ],
+      [ "INIT_PARAMS", "structlogog_1_1_i_n_i_t___p_a_r_a_m_s.html", null ],
+      [ "Object", "classlogog_1_1_object.html", [
+        [ "Formatter", "classlogog_1_1_formatter.html", [
+          [ "FormatterGCC", "classlogog_1_1_formatter_g_c_c.html", null ],
+          [ "FormatterMSVC", "classlogog_1_1_formatter_m_s_v_c.html", null ]
+        ] ],
+        [ "Mutex", "classlogog_1_1_mutex.html", [
+          [ "LockableNodesType", "classlogog_1_1_lockable_nodes_type.html", null ]
+        ] ],
+        [ "Node", "classlogog_1_1_node.html", [
+          [ "Topic", "classlogog_1_1_topic.html", [
+            [ "Filter", "classlogog_1_1_filter.html", null ],
+            [ "TopicGroup", "classlogog_1_1_topic_group.html", null ],
+            [ "TopicLevel", "classlogog_1_1_topic_level.html", null ],
+            [ "TopicSink", "classlogog_1_1_topic_sink.html", [
+              [ "Target", "classlogog_1_1_target.html", [
+                [ "Cerr", "classlogog_1_1_cerr.html", null ],
+                [ "Cout", "classlogog_1_1_cout.html", null ],
+                [ "LogOutput", "classlogog_1_1_log_output.html", null ],
+                [ "OutputDebug", "classlogog_1_1_output_debug.html", null ],
+                [ "Socket", "classlogog_1_1_socket.html", [
+                  [ "SocketServer", "classlogog_1_1_socket_server.html", null ]
+                ] ]
+              ] ]
+            ] ],
+            [ "TopicSource", "classlogog_1_1_topic_source.html", [
+              [ "Checkpoint", "classlogog_1_1_checkpoint.html", [
+                [ "Message", "classlogog_1_1_message.html", null ]
+              ] ]
+            ] ]
+          ] ]
+        ] ],
+        [ "ScopedLock", "classlogog_1_1_scoped_lock.html", null ],
+        [ "String", "classlogog_1_1_string.html", null ],
+        [ "Timer", "classlogog_1_1_timer.html", null ]
+      ] ],
+      [ "Allocator< T >::rebind< U >", "structlogog_1_1_allocator_1_1rebind.html", null ],
+      [ "Statics", "classlogog_1_1_statics.html", null ],
+      [ "TestSignup", "classlogog_1_1_test_signup.html", null ],
+      [ "Thread", "classlogog_1_1_thread.html", null ],
+      [ "UnitTest", "classlogog_1_1_unit_test.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "logog", "namespacelogog.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "include/api.hpp", "api_8hpp.html", null ],
+      [ "include/checkpoint.hpp", "checkpoint_8hpp.html", null ],
+      [ "include/formatter.hpp", "formatter_8hpp.html", null ],
+      [ "include/logog.hpp", "logog_8hpp.html", null ],
+      [ "include/macro.hpp", "macro_8hpp.html", null ],
+      [ "include/message.hpp", "message_8hpp.html", null ],
+      [ "include/mutex.hpp", "mutex_8hpp.html", null ],
+      [ "include/node.hpp", "node_8hpp.html", null ],
+      [ "include/object.hpp", "object_8hpp.html", null ],
+      [ "include/platform.hpp", "platform_8hpp.html", null ],
+      [ "include/socket.hpp", "socket_8hpp.html", null ],
+      [ "include/statics.hpp", "statics_8hpp.html", null ],
+      [ "include/string.hpp", "string_8hpp.html", null ],
+      [ "include/target.hpp", "target_8hpp.html", null ],
+      [ "include/thread.hpp", "thread_8hpp.html", null ],
+      [ "include/timer.hpp", "timer_8hpp.html", null ],
+      [ "include/topic.hpp", "topic_8hpp.html", null ],
+      [ "include/unittest.hpp", "unittest_8hpp.html", null ],
+      [ "test/test.cpp", "test_8cpp.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
