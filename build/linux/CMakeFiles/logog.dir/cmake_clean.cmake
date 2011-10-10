904b3c6b966b5599c1fc5ea6018683e8e352f80a
John Byrd <jbyrd@giganticsoftware.com>
2011-10-10 13:30:15 -0700

    cmake project generated for linux.

---
+++ build/linux/CMakeFiles/logog.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/logog.dir/src/api.cpp.o"
+  "CMakeFiles/logog.dir/src/checkpoint.cpp.o"
+  "CMakeFiles/logog.dir/src/formatter.cpp.o"
+  "CMakeFiles/logog.dir/src/lobject.cpp.o"
+  "CMakeFiles/logog.dir/src/lstring.cpp.o"
+  "CMakeFiles/logog.dir/src/message.cpp.o"
+  "CMakeFiles/logog.dir/src/mutex.cpp.o"
+  "CMakeFiles/logog.dir/src/node.cpp.o"
+  "CMakeFiles/logog.dir/src/platform.cpp.o"
+  "CMakeFiles/logog.dir/src/socket.cpp.o"
+  "CMakeFiles/logog.dir/src/statics.cpp.o"
+  "CMakeFiles/logog.dir/src/target.cpp.o"
+  "CMakeFiles/logog.dir/src/timer.cpp.o"
+  "CMakeFiles/logog.dir/src/topic.cpp.o"
+  "CMakeFiles/logog.dir/src/unittest.cpp.o"
+  "liblogog.pdb"
+  "liblogog.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/logog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
